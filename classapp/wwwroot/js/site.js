b57092ca0ea670a1adfcc64f6ea8bad67f6c2752
ethannance <ethannance523@gmail.com>
2024-02-09 12:17:28 -0700

    edited comments

--- classapp/wwwroot/js/site.js
+++ classapp/wwwroot/js/site.js
@@ -1,7 +1,5 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+// This code creates a calculator using jquery to multiply hours and rates.
 $(document).ready(function () {
     $("#calculateButton").off().click(function () {
         var hours = $("#hours").val();
